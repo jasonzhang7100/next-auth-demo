b4e05537f1e6800e9914e86b2c6f129e98aa979c
Jason Zhang <jasonzhang.syd@gmail.com>
2021-07-25 22:12:27 +1000

    set GitHub and Twitter Providers

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,28 @@
+import NextAuth from 'next-auth';
+import Providers from 'next-auth/providers';
+
+const options = {
+  providers: [
+    Providers.GitHub({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+    }),
+    Providers.Twitter({
+      clientId: process.env.TWITTER_ID,
+      clientSecret: process.env.TWITTER_SECRET,
+    }),
+    // Providers.Email({
+    //   server: {
+    //     host: '',
+    //     port: '',
+    //     auth: {
+    //       user: '',
+    //       pass: '',
+    //     },
+    //   },
+    //   from: '',
+    // }),
+  ],
+};
+
+export default (req, res) => NextAuth(req, res, options);
